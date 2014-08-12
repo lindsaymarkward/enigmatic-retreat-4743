202d2b35bdb0649c91b1fe31efda19842af11017
Lindsay Ward <lindsay.ward@jcu.edu.au>
2014-08-12 23:16:15 +1000

    init

---
+++ web.js
@@ -0,0 +1,16 @@
+// https://devcenter.heroku.com/articles/getting-started-with-nodejs
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
